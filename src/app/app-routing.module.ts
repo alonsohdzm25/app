937a909afbe00332463a323c8845afe3e3c0c313
Alonso Hernandez Mora <alonsohdzm25@gmail.com>
2022-10-25 16:37:11 -0500

    Configuracion del archivo app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: AppComponent, pathMatch: "full"},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
