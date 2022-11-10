5ec3fd077c03cfdfdc2717f86c4443d57c36443c
Alonso Hernandez Mora <alonsohdzm25@gmail.com>
2022-11-10 16:34:58 -0600

    Creacion de componentes forget-pass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgetPassComponent } from './forget-pass/forget-pass.component';
 
 const routes: Routes = [
   { path: "", component: AppComponent, pathMatch: "full"},
   { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'forget-password',component: ForgetPassComponent }
 ];
 
 @NgModule({
