40b5d5bbe7f2f35f2e62c1abe2595f545446f60c
Anai <222t0540@itsm.edu.mx>
2022-11-22 17:45:39 -0600

    ine

---
+++ src/app/ine/ine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ine',
+  templateUrl: './ine.component.html',
+  styleUrls: ['./ine.component.css']
+})
+export class IneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
