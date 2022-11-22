d6616dbbee21a9b735419f148906c6ace7a07f13
Anai <222t0540@itsm.edu.mx>
2022-11-22 17:43:25 -0600

    nacimiento

---
+++ src/app/nacimiento/nacimiento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nacimiento',
+  templateUrl: './nacimiento.component.html',
+  styleUrls: ['./nacimiento.component.css']
+})
+export class NacimientoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
