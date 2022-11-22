e72e08da1cbeb13717dd887b7d896ef7f0721e90
Anai <222t0540@itsm.edu.mx>
2022-11-22 17:55:43 -0600

    libros

---
+++ src/app/coautorialibros/coautorialibros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coautorialibros',
+  templateUrl: './coautorialibros.component.html',
+  styleUrls: ['./coautorialibros.component.css']
+})
+export class CoautorialibrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
