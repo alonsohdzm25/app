d395547b678ab71e29b97c99edea444a35c5d0f0
Anai <222t0540@itsm.edu.mx>
2022-11-22 18:08:09 -0600

    agregar-divulgacion-cientifica

---
+++ src/app/agregar-divulgacion-cientifica/agregar-divulgacion-cientifica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-divulgacion-cientifica',
+  templateUrl: './agregar-divulgacion-cientifica.component.html',
+  styleUrls: ['./agregar-divulgacion-cientifica.component.css']
+})
+export class AgregarDivulgacionCientificaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
