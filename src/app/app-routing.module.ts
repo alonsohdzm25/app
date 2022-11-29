ae5a96f776199d87d929649956b8078884758cda
Anai <222t0540@itsm.edu.mx>
2022-11-29 13:41:05 -0600

    general-data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgetPassComponent } from './forget-pass/forget-pass.component';
+import { GeneralDataComponent } from './general-data/general-data.component';
+import { NacimientoComponent } from './nacimiento/nacimiento.component';
 
 const routes: Routes = [
   { path: "", component: AppComponent, pathMatch: "full"},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent },
-  { path: 'forget-password',component: ForgetPassComponent }
+  { path: 'forget-password',component: ForgetPassComponent },
+  { path: 'general-data', component: GeneralDataComponent},
+  {path: 'nacimiento', component: NacimientoComponent}
 ];
 
 @NgModule({
